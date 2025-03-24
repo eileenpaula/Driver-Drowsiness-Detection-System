6221a9cfb5b68ff30fb1a7dc26db14ccb22097a9
Ronaldo Carrazco <racarrazco@gmail.com>
2025-03-24 01:00:55 -0400

    camera auto-starts and repeats needs waitTime and recordDuration linked to backend

--- backend/app/__inti__.py
+++ backend/app/__inti__.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 CORS(app)
 
 # Define the folder where uploaded files will be saved
-UPLOAD_FOLDER = '<YOUR DOWNLOAD FOLDER>'
+UPLOAD_FOLDER = 'DOWNLOAD_PATH'
 os.makedirs(UPLOAD_FOLDER, exist_ok=True)
 
 # Configure Flask to save uploaded files in the specified folder
@@ -35,64 +35,51 @@ def upload_video():
     
     ################################
 
-    cap = cv2.VideoCapture(file_path)
+    # cap = cv2.VideoCapture(file_path)
 
-    # Chqeck if camera opened successfully
-    if (cap.isOpened()== False):
-        print("Error opening video file")
+    # # Chqeck if camera opened successfully
+    # if (cap.isOpened()== False):
+    #     print("Error opening video file")
 
-    # Read until video is completed
-    while(cap.isOpened()):
+    # # Read until video is completed
+    # while(cap.isOpened()):
         
-    # Capture frame-by-frame
-        ret, frame = cap.read()
-        if ret == True:
-        # Display the resulting frame
-            cv2.imshow('Frame', frame)
+    # # Capture frame-by-frame
+    #     ret, frame = cap.read()
+    #     if ret == True:
+    #     # Display the resulting frame
+    #         cv2.imshow('Frame', frame)
             
-        # Press Q on keyboard to exit
-            if cv2.waitKey(25) & 0xFF == ord('q'):
-                break
+    #     # Press Q on keyboard to exit
+    #         if cv2.waitKey(25) & 0xFF == ord('q'):
+    #             break
 
-    # Break the loop
-        else:
-            break
+    # # Break the loop
+    #     else:
+    #         break
 
-    # When everything done, release
-    # the video capture object
-    cap.release()
+    # # When everything done, release
+    # # the video capture object
+    # cap.release()
 
-    # Closes all the frames
-    cv2.destroyAllWindows()
+    # # Closes all the frames
+    # cv2.destroyAllWindows()
 
 
-    ################################
+    ###############################
 
 
 
     return jsonify({"message": "Video uploaded successfully", "file_path": file_path}), 200
 
-@app.route('/downloads/videotest', methods=['GET'])
-def download_file(filename):
-    # Provide the file to the user for download
-    return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
-
-@app.route("/video", methods=['GET', 'POST'])
-def video():
-    if(request.method == "POST"):
-        bytesOfVideo = request.get_data()
-        with open('video.mp4', 'wb') as out:
-            out.write(bytesOfVideo)
-        return "Video read"
-
 @app.route('/data', methods=['GET'])
 def get_data():
     # Sample data to send to the frontend
-    i = randint(1,10)
+    i = randint(5,15)
     print(i)
     data = {
         'status': 'success',
-        'id': 10
+        'waitDuration': i
     }
     
     # Return the data as a JSON response
