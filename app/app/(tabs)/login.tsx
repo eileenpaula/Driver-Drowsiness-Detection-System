9b62a25b56d5a35b8d4225fc760a3376fe880871
Oscar Alvarez <67487448+oscaralvarez711@users.noreply.github.com>
2025-03-28 20:35:30 -0400

    Update login.tsx

--- app/app/(tabs)/login.tsx
+++ app/app/(tabs)/login.tsx
@@ -3,6 +3,8 @@ import { View, Text, TextInput, TouchableOpacity, StyleSheet, Alert } from 'reac
 import { FIREBASE_AUTH } from '../firebase_config';
 import { signInWithEmailAndPassword, createUserWithEmailAndPassword } from 'firebase/auth';
 import { useRouter } from 'expo-router';
+import { setDoc, doc } from "firebase/firestore";
+import { auth, db } from "./firebaseConfig";
 
 const Login = () => {
   const [email, setEmail] = useState('');
