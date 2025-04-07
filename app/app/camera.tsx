62e8a373a220b4b5e0580921eea25a6fe5210cb7
Ronaldo Carrazco <racarrazco@gmail.com>
2025-04-06 23:28:14 -0400

    save videos straight to firestorage

--- app/app/camera.tsx
+++ app/app/camera.tsx
@@ -10,14 +10,17 @@ import { Link } from 'expo-router';
 import {Ionicons} from '@expo/vector-icons';
 import * as Progress from "react-native-progress";
 import { useNavigation } from "@react-navigation/native";
-import { DocumentData } from "firebase/firestore"; 
+import { DocumentData, setDoc, doc } from "firebase/firestore"; 
 import { FIREBASE_AUTH, FIREBASE_DB } from "../database/.config";
 import { Audio } from 'expo-av';
+import { getStorage, ref, uploadBytes, uploadBytesResumable } from 'firebase/storage'
+import {v4 as uuidv4} from 'uuid'
+import uuid from 'react-native-uuid';
 
 export default function App() {
   const navigation = useNavigation();
   const [permission, requestPermission] = useCameraPermissions();
-  const ref = useRef<CameraView>(null);
+  const camRef = useRef<CameraView>(null);
   const [mode, setMode] = useState<CameraMode>("video");
   const [facing, setFacing] = useState<CameraType>("front");
   const [recording, setRecording] = useState(false);
@@ -95,6 +98,30 @@ export default function App() {
       }
   };
 
+  const send_to_storage = (uri: string) => {
+    const file_path =  `videos/${activeUser.uid}/${uuid.v4()}.mov`
+    const storage = getStorage()
+    const videoRef = ref(storage, file_path)
+
+      // React Native requires a specific structure for file uploads
+    const blub = new Blob([uri],{type: 'video/mp4'})
+    const file = uploadBytesResumable(videoRef, blub)//{
+
+      // Append the file to FormData
+    file.on('state_changed',
+      (snapshot) =>{
+          const progress = (snapshot.bytesTransferred /snapshot.totalBytes) * 100
+          console.log(`upload is ${progress.toFixed(2)}% done`)
+      }
+    )
+      
+    setDoc(doc(FIREBASE_DB, "users", activeUser.uid,'videos', uuid.v4()), {
+      "file_path": file_path,
+      "time_stored": Date.now()
+    });
+
+  }
+
   const fetchDataFromBackend = async () => {
     try {
       // Replace <your-ip> with your local network IP address
@@ -127,9 +154,10 @@ export default function App() {
       } else {
         setRecording(true);
         console.log("New recording started...");
-        const video = await ref.current?.recordAsync();
+        const video = await camRef.current?.recordAsync();
         console.log({ video });
-        await sendVideoToBackend(video?.uri || "")
+        // await sendVideoToBackend(video?.uri || "")
+        send_to_storage(video?.uri || "")
       }
     }else{
       console.log("Permission not granted to record video")
@@ -139,7 +167,7 @@ export default function App() {
   const stopRecording = () => {
     setRecording(false);
     console.log("Recording stopped.");
-    ref.current?.stopRecording();
+    camRef.current?.stopRecording();
   };
 
   /** First useEffect hook listens for changes to certain state vars that are changed (wiating, recordin, or waitDuration)
@@ -234,7 +262,7 @@ export default function App() {
     return (
       <CameraView
         style={styles.camera}
-        ref={ref}
+        ref={camRef}
         mode={mode}
         facing={facing}
         mute={false}
