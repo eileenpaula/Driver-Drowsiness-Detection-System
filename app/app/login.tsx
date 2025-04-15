bd778c1118c9ede574a5fbb495e3877e36d3acfe
Ronaldo Carrazco <racarrazco@gmail.com>
2025-04-14 23:15:31 -0400

    theme small fix

--- app/app/login.tsx
+++ app/app/login.tsx
@@ -1,10 +1,16 @@
 import React, { useState } from 'react';
-import { View, Text, TextInput, TouchableOpacity, StyleSheet, Alert } from 'react-native';
+import { View, Text, Image, TextInput, TouchableOpacity, StyleSheet, Alert } from 'react-native';
 import { useRouter } from 'expo-router';
 import { Link } from 'expo-router';
 import { login_user } from '@/database/user_session';
+import {useFonts} from 'expo-font';
 
 const login = () => {
+  const [loadFonts]=useFonts({
+      'IS': require('../assets/fonts/Instrument_Sans.ttf'),
+      'ISBold': require('../assets/fonts/InstrumentSans-Bold.ttf'),
+    })
+
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [loading, setLoading] = useState(false);
@@ -25,7 +31,15 @@ const login = () => {
 
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Drowsy Driver Detection System</Text>
+
+      <View style={styles.logoContainer}>
+        <Image source={require("../assets/images/logo2.png")} style={styles.logo} />
+      </View>
+      {/* <View>
+      <Text style={styles.title}>Drowsy Driver Detection{'\n'}
+      <Text style={{ color: '#99342C' }}>System</Text>
+      </Text>
+      </View> */}
       <Text style={styles.subHeading}>Login</Text>
       
       <TextInput
@@ -67,15 +81,35 @@ const login = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    // alignItems: 'center',
     justifyContent: 'center',
     padding: 20,
-    backgroundColor: '#f8f9fa',
+    backgroundColor: 'rgb(206, 209, 184)',
+  },
+  logo: {
+    width: 255,
+    height: 130,
+    resizeMode: "contain",
+    // alignItems:'center',
+    // justifyContent: 'center',
+  },
+  logoContainer: {
+    // flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "rgb(206, 209, 184)",
+    marginBottom: 30
+    
   },
   title: {
-    fontSize: 28,
-    fontWeight: 'bold',
-    marginBottom: 20,
-    textAlign: 'center',
+    fontFamily: "ISBold",
+    //color: '#99342C',
+    fontSize: 25,
+    textAlign: "center",
+    letterSpacing: 0.5,
+    textTransform: 'uppercase',
+    fontWeight: "bold",
+    marginBottom: 60,
   },
   subHeading: {
     fontSize: 20,
@@ -88,9 +122,9 @@ const styles = StyleSheet.create({
     marginVertical: 10,
     padding: 15,
     borderWidth: 1,
-    borderColor: '#ccc',
+    borderColor: 'rgb(163, 165, 146)',
     borderRadius: 10,
-    backgroundColor: '#fff',
+    backgroundColor: 'rgb(193, 196, 168)',
   },
   button: {
     marginTop: 20,
@@ -100,7 +134,7 @@ const styles = StyleSheet.create({
     alignItems: 'center',
   },
   loginButton: {
-    backgroundColor: '#FF5555',
+    backgroundColor: '#99342C',
   },
   buttonText: {
     color: '#fff',
@@ -110,7 +144,7 @@ const styles = StyleSheet.create({
   signUpButtonText:{
     textAlign: 'center',
     padding: 25,
-    color: '#FF5555',
+    color: '#99342C',
     fontWeight: 'bold',
     fontSize: 18,
   },
